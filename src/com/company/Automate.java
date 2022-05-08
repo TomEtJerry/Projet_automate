cc50be3533b3c81158350f61cc90d537899b8ee5
TomEtJerry <78385156+TomEtJerry@users.noreply.github.com>
2022-05-08 16:20:53 +0200

    Add files via upload

--- src/com/company/Automate.java
+++ src/com/company/Automate.java
@@ -1,18 +1,19 @@
 package com.company;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 public class Automate {
     private final static char [] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-    private int nbr_symbole;
+    private final int nbr_symbole;
     private int nbr_etat;
     private int [] etat;
-    private int nbr_etat_init;
-    private int [] etat_init;
-    private int nbr_etat_fin;
-    private int [] etat_fin;
+    private final int nbr_etat_init;
+    private final int [] etat_init;
+    private final int nbr_etat_fin;
+    private final int [] etat_fin;
     private int nbr_trans;
     private String [] transition;
 
@@ -81,15 +82,15 @@ public class Automate {
 
     public static boolean Test_determinisation(Automate automate){
         int i=0;
-        int j=1;
+        int j;
         if (automate.nbr_etat_init > 1) {
             return false;
         }
         while(i< automate.getNbr_trans()){
-            j = i + 1;
+            j=0;
             while(j< automate.getNbr_trans()){
                 //si l'état et la lettre sont les mêmes alors non déterministe
-                if(automate.getTransition()[i].charAt(0) == automate.getTransition()[j].charAt(0) && automate.getAlphabet()[i] == automate.getAlphabet()[j]){
+                if(automate.getTransition()[i].charAt(0) == automate.getTransition()[j].charAt(0) && automate.getTransition()[i].charAt(1) == automate.getTransition()[j].charAt(1) && i != j){
                     return false;
                 }
                 j++;
@@ -101,14 +102,15 @@ public class Automate {
 
     public static boolean Test_completion(Automate automate){
         int i = 0;
-        int j = 1;
-        int cpt = 0;
-        // i et j comptent le nombre de fois qu'il y a un état
-        while (i< automate.getNbr_trans()){
-            j = i + 1;
+        int j;
+        int cpt;
+
+        while (i< automate.getNbr_etat()){
+            j=0;
+            cpt = 0;
             while (j< automate.getNbr_trans()){
-                if(automate.getTransition()[i].charAt(0) == automate.getTransition()[j].charAt(0)){
-                    cpt++;
+                if(automate.getTransition()[j].charAt(0) == automate.getEtat()[i]){
+                    cpt += 1;
                 }
                 j++;
             }
@@ -121,125 +123,141 @@ public class Automate {
         return true;
     }
 
-    public static Automate Completion(Automate automate) {
-        automate.nbr_symbole += 1;
-        automate.nbr_etat += 1;
+    public void completion() {
+        this.nbr_etat += 1;
         String [] transition_C = new String[50];
-        automate.etat = new int[automate.nbr_etat]; //tableau statique qui contient tout les etats
+        this.etat = new int[this.nbr_etat]; //tableau statique qui contient tout les etats avec l'etat poubelle en plus
         int e = 0;
-        while(e<automate.nbr_etat){
-            automate.etat[e] = e;
+        while(e<this.nbr_etat){
+            this.etat[e] = e;
             e++;
         }
-        automate.etat[automate.nbr_etat-1] = 0;  // Vérifier l'indice + modifier tableau d'entiers
+        this.etat[this.nbr_etat-1] = -1;  // Vérifier l'indice + modifier tableau d'entiers
         int i = 0;
-        int j = 0;
-        char[] alpha = automate.getAlphabet();
-        char x;
-        // Parcours les états
-        while (i < automate.getNbr_trans()) {
-            //Parcours les symboles
-            while (j < automate.getNbr_symbole()) {
-                x = alpha[j];
-                // Si l'état possède une transition avec ce symbole ajouter au string sinon créer et ajouter
-                if (automate.getTransition()[i].charAt(1) == x) {
-                    transition_C[j] = String.valueOf(automate.getTransition()[i].charAt(1) + x + automate.getTransition()[i].charAt(2));
-                } else {
-                    automate.nbr_trans++;
-                    transition_C[j] = String.valueOf(automate.getTransition()[i].charAt(1) + x + "p");
+        int j;
+        int l;
+        int k = 0; //k va s'incrementer pour le nouveau tableau de transition
+        boolean test; //sert à savoir si la transition contient l'un des symbole
+        int nbr_transition = this.nbr_trans;
+
+        while(i < this.nbr_etat-1){
+            j=0;
+            while(j < nbr_symbole){
+                l=0;
+                test = false;
+                while(l < nbr_transition){
+                    if(this.etat[i] == Integer.parseInt(String.valueOf(this.transition[l].charAt(0))) && this.getAlphabet()[j] == this.transition[l].charAt(1)){
+                        transition_C[k] = this.transition[l];
+                        k++;
+                        test = true;
+                        break;
+                    }
+                    l++;
+                }
+                if(!test){
+                    transition_C[k] = String.valueOf(i) + this.getAlphabet()[j] + "p";
+                    k++;
                 }
                 j++;
             }
             i++;
         }
-        return new Automate(automate.nbr_symbole, automate.nbr_etat, automate.etat, automate.nbr_etat_init, automate.etat_init, automate.nbr_etat_fin, automate.etat_fin, automate.nbr_trans,  transition_C);
+        j=0;
+        while(j < nbr_symbole){
+            transition_C[k] = "-1" + this.getAlphabet()[j] + "p";
+            k++;
+            j++;
+        }
+
+        this.nbr_trans = k;
+        this.transition = transition_C;
     }
 
 
-    public static Automate Determinisation(Automate automate){
-        int i = 1;
+    public Automate determinisation(){
+        int i = 0;
         //état initial
-            //Concatène les états initiaux
-        String letat_init_D = String.valueOf(automate.etat_init[0]);
-        while(i< automate.nbr_etat_init){
-            letat_init_D += automate.etat_init[i];
+        //Concatène les états initiaux
+        String letat_init_D = "";
+        while(i < this.nbr_etat_init){
+            letat_init_D += String.valueOf(this.etat_init[i]);
+            i++;
         }
         int nbr_etat_init_D = 1;
-        int [] etat_init_D = new int[0];
-        etat_init_D[0] = Integer.parseInt(letat_init_D);
+        int [] etat_init_D = {Integer.parseInt(""+letat_init_D)};
+
 
         //états
-        String nv_etat = ""; // Contient le nouvel état d'arrivé (transition)
-        int nbr_etat_C = 1;
+        int nbr_etat_D = 1;
         int [] etat_D = new int[50];
-        String [] transition_D = new String[50];
-        etat_D[0] = Integer.parseInt(letat_init_D);
-        transition_D[0]= letat_init_D; //Premier état
-        int t = 0; // Parcours le nouveau tableau d'états (compare)
-        int k; // Parcours le nouvel état (chaine de caractère en tableau)
-        int a; // Parcours l'alphabet
-        int u; // Parcours l'ancien tableau de transition
-        int s = 0; // Parcours l'ancienne transition (chaine de caractère en tableau)
-        int nbr_trans_D = 0; // Parcours le nouveau tableau de transition
-        while (transition_D[t] != null){
-            k =0;
-            while ( k < String.valueOf(etat_D[t]).length()){ // Taille de l'état à parcourir
-                u = 0;
-                while (u < automate.getNbr_trans()){
-                    a = 0;
-                    while (a < automate.nbr_symbole){
-                        if (automate.getTransition()[u].charAt(0) == String.valueOf(etat_D[t]).charAt(k)){
-                            if (automate.getTransition()[u].charAt(1) == automate.getAlphabet()[a]){
-                                nv_etat += automate.getTransition()[u].charAt(2);
-                                transition_D[nbr_trans_D] = etat_D[t] + automate.getAlphabet()[a] + nv_etat; // Nouvelle transition
-                                nbr_trans_D++;
-                            }
+        Arrays.fill(etat_D, -1);
+        etat_D[0] = Integer.parseInt(letat_init_D); // premier etat qui est l'etat initial fait avant
+
+        int nbr_trans_D = 0; // nouveau nbr de transitions
+        String [] transition_D = new String[50]; // nouveau tableau de transition
+
+        i = 0; // parcours le nouveau tableau d'état
+        int j; // parcours
+        int k; //
+        int l;
+
+        String nv_etat; // Contient le nouvel état d'arrivé (transition)
+        while(etat_D[i] != -1){
+            j=0;
+            while(j < this.nbr_symbole){
+                k=0;
+                nv_etat = "";
+                while (k < this.nbr_trans){
+                    l=0;
+                    while (l < String.valueOf(etat_D[i]).length()){
+                        if(transition[k].charAt(0) == String.valueOf(etat_D[i]).charAt(l) && transition[k].charAt(1) == alphabet[j]){
+                            nv_etat += transition[k].charAt(2);
                         }
-                        a++;
+                        l++;
                     }
-                    etat_D[nbr_etat_C] = Integer.parseInt(nv_etat);
-                    nv_etat = "";
-                    a = 0;
-                    nbr_etat_C++;
-                    u++;
+                    k++;
                 }
-                k++;
+                if(!nv_etat.equals("") && !Arrays.toString(etat_D).contains(nv_etat)){
+                    etat_D[nbr_etat_D] = Integer.parseInt(nv_etat);
+                    nbr_etat_D += 1;
+                    transition_D[nbr_trans_D] = String.valueOf(etat_D[i])+alphabet[j]+nv_etat;
+                    nbr_trans_D += 1;
+                }
+                else if(!nv_etat.equals("")){
+                    transition_D[nbr_trans_D] = String.valueOf(etat_D[i])+alphabet[j]+nv_etat;
+                    nbr_trans_D += 1;
+                }
+                j++;
             }
-            t++;
+            i++;
         }
 
-        //Etats finaux
+        //etat finaux
+        i = 0; //parcours des nouveaux état
+        j = 0; // parcours des chiffres de l'état pour savoir si un d'eux est fina
         int nbr_etat_fin_D = 0;
-        int [] etat_fin_D = new int[50];
-        int v = 0; // Parcours le nouveau tableau des états finaux (comparer)
-        int c; // Parcours l'ancien tableau des états finaux
-        t = 0;
-        int comp; // Savoir si la valeur est dans le tableau
-        while (transition_D[t] != null){
-            k = 0;
-            while ( k < String.valueOf(etat_D[t]).length()){
-                c = 0;
-                while(c < automate.etat_fin.length){
-                    if(String.valueOf(etat_D[t]).charAt(k) == automate.etat_fin[c]){
-                        comp = 0;
-                        while(etat_fin_D[v] != 0){ // Verifie que l'état n'est pas déjà dans le tableau
-                            if (etat_fin_D[v] == etat_D[t]){
-                                comp ++;
-                            }
-                            v++;
-                        }
-                        if (comp == 0){
-                            etat_fin_D[nbr_etat_fin_D] = etat_D[t];
-                            nbr_etat_fin_D ++;
-                        }
-                    }
-                    c++;
+        int [] temp = new int[50];
+
+        while(i < nbr_etat_D){
+            j=0;
+            while (j < String.valueOf(etat_D[i]).length()){
+                if (Arrays.toString(etat_fin).contains(String.valueOf(String.valueOf(etat_D[i]).charAt(j)))){ // si on trouve un des caracteres dans le tableau d'etat finaux de base, on met en sortie
+                    temp[nbr_etat_fin_D] = etat_D[i];
+                    nbr_etat_fin_D += 1;
+                    break;
                 }
-                k++;
+                j++;
             }
-            t++;
+            i++;
         }
 
-        return new Automate(automate.nbr_symbole,nbr_etat_C , etat_D, nbr_etat_init_D, etat_init_D, nbr_etat_fin_D, etat_fin_D, nbr_trans_D, transition_D) ;
+        k=0; //remplissage du tableau d'etat finaux
+        int [] etat_fin_D = new int[nbr_etat_fin_D];
+        while(k < nbr_etat_fin_D){
+            etat_fin_D[k] = temp[k];
+            k++;
+        }
+
+        return new Automate(this.nbr_symbole, nbr_etat_D , etat_D, nbr_etat_init_D, etat_init_D, nbr_etat_fin_D, etat_fin_D, nbr_trans_D, transition_D);
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,20 @@
 package com.company;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws FileNotFoundException {
         Automate automate = Automate_affichage.lecture("automate_test.txt");
-        int i=0;
         Automate_affichage.afficher_automate(automate);
-        System.out.println(Automate.Test_asynchrone(automate));
         System.out.println(Automate.Test_determinisation(automate));
-        System.out.println(Automate.Test_completion(automate));
+        Automate auto_det = automate.determinisation();
+        System.out.println(auto_det.getTransition()[1]);
+
+
+
         //Automate_affichage.afficher_automate(Automate.Completion(automate));
     }
 }
